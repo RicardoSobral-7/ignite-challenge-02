a1ae856e2ce640a59479844dd4d00488e52284c9
Ricardo Sobral <37637218+RicardoSobral-7@users.noreply.github.com>
2021-03-15 10:21:22 -0300

    desafio

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,18 @@
-export function Content() {
+export function Content({ movies, MovieCard, selectedGenre }) {
   // Complete aqui
+  return (
+    <>
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </>
+  )
 }
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,27 @@
-export function SideBar() {
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+export function SideBar({ genres, Button, selectedGenreId, handleClickButton }) {
   // Complete aqui
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {genres.map(genre => (
+          <Button
+            id={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => handleClickButton(genre.id)}
+            selected={selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
